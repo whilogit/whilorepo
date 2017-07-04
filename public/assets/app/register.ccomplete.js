66024f793a28e5c0e3c8aa4e8f5e475b964d3e2c
Vinod VG <vgvinodvg@gmai.com>
2017-07-04 14:35:23 +0530

    registration steps updates

--- public/assets/app/register.ccomplete.js
+++ public/assets/app/register.ccomplete.js
@@ -1,21 +1,14 @@
 $("#complete_btn").click(function() { 
+
   var postdata = {};
                                 postdata['_token'] = $('meta[name="csrf-token"]').attr('content'); 
                                 postdata['companyid']=$('input[name="companyid"]').val();
                                 postdata['emailAdd']=$('input[name="emailAdd"]').val();;
                                
                                $('body').removeClass('loaded');
-                                                        $.post('send/registrationmail',postdata,function(response){ 
-
-                                                        $('body').addClass('loaded');
-                                                                if(response.success)
-                                                                {
-                                                                    $.alert('EmailSend!');
-                                                                   $('#search_email_button_'+response.buttonid).remove();
-                                                                   $('#statustd_'+response.buttonid).append('<span><b>Email Sent</b></span>');
+                                                        $.post('/send/registrationmail',postdata,function(response){ 
                                                                    
-                                                                    
-                                                                }
+                                                     
                                                 });
 
 
