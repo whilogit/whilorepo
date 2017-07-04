9cb36c0e1b1820c082c318d26f3b985b48d8dfc1
anujavinod <anujavinodvg@gmail.com>
2017-07-04 13:29:11 +0530

    complete registration steps changes

--- public/assets/app/register.ccomplete.js
+++ public/assets/app/register.ccomplete.js
@@ -1,31 +1,22 @@
-$(function(){ 
-	$.ccomplete = {
-		init: function(callback) {
-			$("[name=ccomplete] .btnsubmit").click(function() { $.ccomplete.complete($(this),callback); });
-		},
-		complete:function(ths,callback){
-		   var postdata = {};
-		   postdata['_token'] = $('meta[name="csrf-token"]').attr('content'); 
-			$('body').removeClass('loaded');
-			$.post('/auth/ccomplete',postdata,function(response){   
-			$('body').addClass('loaded');
-				if(response.success)
-					location.href = "/myaccount";
-				else 
-					{
-					if ((typeof  response.errors) == 'object') { 
-						var errorsHtml = ""; 
-						$.each( response.errors, function( key, value ) {
-							errorsHtml += '<li><i class="fa fa-times" style="color:#F00;"></i>' + value[0] + '</li>';
-						});
-						$('[name=company]  .responsereport li').html('' + errorsHtml);
-					}else{
-						$('[name=company]  .responsereport li').html('' + response.errors);
-					}
-					
-					}
-			},'json');
-		},
-	}
-	$.ccomplete.init();
-})(jQuery);
+$("#complete_btn").click(function() { 
+  var postdata = {};
+                                postdata['_token'] = $('meta[name="csrf-token"]').attr('content'); 
+                                postdata['companyid']=$('input[name="companyid"]').val();
+                                postdata['emailAdd']=$('input[name="emailAdd"]').val();;
+                               
+                               $('body').removeClass('loaded');
+                                                        $.post('send/registrationmail',postdata,function(response){ 
+
+                                                        $('body').addClass('loaded');
+                                                                if(response.success)
+                                                                {
+                                                                    $.alert('EmailSend!');
+                                                                   $('#search_email_button_'+response.buttonid).remove();
+                                                                   $('#statustd_'+response.buttonid).append('<span><b>Email Sent</b></span>');
+                                                                   
+                                                                    
+                                                                }
+                                                });
+
+
+            });
--- public/assets/app/register.companyplan.js
+++ public/assets/app/register.companyplan.js
@@ -62,12 +62,46 @@ $(function(){
 });
     $('#enterprisebtn').on('click', function() {
 
-     var enterprise = $("#enterprisePlan").serialize();
+      var postdata = {}
+       var input = $('[name=expressPlan]').serialize();
+      postdata['_token'] = $('meta[name="csrf-token"]').attr('content'); 
+      postdata['companyId']=$('input[name="companyid"]').val();
+      postdata['planId']=$('input[name="planid"]').val();
+       $('body').removeClass('loaded');
+        $.post('/company/payment',postdata,function(response){   
+                                         $('body').addClass('loaded');
+                                                 if(response.success)
+                                                 {
+
+                                                     $('#append_ccavenu').html(response);
+                                                    $('#ccavenu_form').submit();
+                                                   
+                                                   
+
+                                                 }
+                                                 });
        
 });
 $('#exclusivebtn').on('click', function() {
 
-     var exclusive = $("#exclusivePlan").serialize();
+       var postdata = {}
+       var input = $('[name=expressPlan]').serialize();
+      postdata['_token'] = $('meta[name="csrf-token"]').attr('content'); 
+      postdata['companyId']=$('input[name="companyid"]').val();
+      postdata['planId']=$('input[name="planid"]').val();
+       $('body').removeClass('loaded');
+        $.post('/company/payment',postdata,function(response){   
+                                         $('body').addClass('loaded');
+                                                 if(response.success)
+                                                 {
+
+                                                     $('#append_ccavenu').html(response);
+                                                    $('#ccavenu_form').submit();
+                                                   
+                                                   
+
+                                                 }
+                                                 });
        
 });
     
