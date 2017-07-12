d705622bf42cb36a88f31bf15223a2c5abae8c60
anujavinod <anujavinodvg@gmail.com>
2017-07-12 21:05:03 +0530

    change in design

--- resources/views/backend/basetables/location.blade.php
+++ resources/views/backend/basetables/location.blade.php
@@ -40,7 +40,7 @@
                             </div>
                         </div>
              
-                       <div id="location_form" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">                    
+                       <div id="location_form" style="margin-top:50px; display:none" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">                    
                         <div class="panel panel-info" >
                                 <div class="panel-heading">
                                     <div class="panel-title">Add New Location</div>
